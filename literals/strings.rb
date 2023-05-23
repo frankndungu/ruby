44ec63459db0da9d83e8516bc1f1490f2dbd2ed6
frankndungu <ndungufrank01@gmail.com>
2023-05-23 11:19:45 +0300

    strings

---
+++ literals/strings.rb
@@ -0,0 +1,8 @@
+# Strings are sequences of characters
+
+puts "I'm on unavailable dem no dey see me"
+puts 'The man said, \'Hi there!\''
+
+# String interpolation
+a = 'ten'
+puts "my favorite number is #{a}!"
